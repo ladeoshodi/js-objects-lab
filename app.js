4a90e2d866b2b92cbad16ac05c7f414956483a1f
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-08-05 13:17:42 +0100

    Exercise 6 completed

--- app.js
+++ app.js
@@ -71,4 +71,20 @@ game.party.push(
     return pokemon.type === "electric" && pokemon.hp >= 70;
   })
 );
+
+/*
+Exercise 6
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+
+
+Solve Exercise 6 here:
+*/
+
+game.gyms.forEach((gym) => {
+  if (gym.difficulty < 3) {
+    gym.completed = true;
+  }
+});
+
 console.log("Game data:", game);

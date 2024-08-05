0d72fe7f0bef14d737d249aaa208f49c19addc45
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-08-05 13:51:01 +0100

    Exercise 8 completed

--- app.js
+++ app.js
@@ -114,4 +114,16 @@ const evolvedPokemon = pokemon.find((pokemon) => {
 
 game.party.splice(0, 1, evolvedPokemon);
 
+/*
+Exercise 8
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 8 here:
+*/
+
+game.party.forEach((pokemon) => {
+  console.log(pokemon.name);
+});
+
 console.log("Game data:", game);

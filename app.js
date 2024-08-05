48b14534798c470567f019b8b42906e87525ede1
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-08-05 14:22:39 +0100

    Exercise 12 completed

--- app.js
+++ app.js
@@ -185,4 +185,18 @@ console.log("Game items");
 console.log(game.items);
 console.log("\n");
 
+/*
+Exercise 12
+1. Similar to Exercise 6, now complete gyms with a difficulty below 6. How will you approach this?
+ (change the value of `complete` in the qualifying objects from false to true).
+
+Solve Exercise 12 here:
+*/
+
+game.gyms.forEach((gym) => {
+  if (gym.difficulty < 6) {
+    gym.completed = true;
+  }
+});
+
 console.log("Game data: \n", game);
